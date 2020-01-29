8373577b6376e811ed6e95ebe6d6685922fda90a
alexyekymov <alexyekymov@gmail.com>
2020-01-29 19:36:38 +0200

    ConsoleHelper implementation.

--- src/com/alex/ConsoleHelper.java
+++ src/com/alex/ConsoleHelper.java
@@ -1,4 +1,38 @@
 package com.alex;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class ConsoleHelper {
+    private static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void writeMessage(String message) {
+        System.out.println(message);
+    }
+
+    public static String readMessage() {
+        String line = null;
+        while (line == null) {
+            try {
+                line = reader.readLine();
+            } catch (IOException e) {
+                System.out.println("Произошла ошибка при попытке ввода текста. Попробуйте еще раз.");
+            }
+        }
+        return line;
+    }
+
+    public static int readInt() {
+        int num;
+        while (true) {
+            try {
+                num = Integer.parseInt(readMessage());
+                break;
+            } catch (NumberFormatException e) {
+                System.out.println("Произошла ошибка при попытке ввода числа. Попробуйте еще раз.");
+            }
+        }
+        return num;
+    }
 }
