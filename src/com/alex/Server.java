4495c38d1407c6e2fa020c1b45cfb9bb603cd0c0
alexyekymov <alexyekymov@gmail.com>
2020-01-31 18:52:01 +0200

    Add server loop.

--- src/com/alex/Server.java
+++ src/com/alex/Server.java
@@ -1,7 +1,28 @@
 package com.alex;
 
-public class Server {
-    public static void main(String[] args) {
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 
+public class Server {
+    public static void main(String[] args) throws IOException {
+        int port = ConsoleHelper.readInt();
+
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+            ConsoleHelper.writeMessage("Сервер запущен.");
+            while (true) {
+                new Handler(serverSocket.accept()).start();
+            }
+        } catch (IOException e) {
+            ConsoleHelper.writeMessage(e.getMessage());
+        }
+    }
+
+    private static class Handler extends Thread {
+        private Socket socket;
+
+        public Handler(Socket socket) {
+            this.socket = socket;
+        }
     }
 }
