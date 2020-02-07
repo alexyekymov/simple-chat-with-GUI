a58b13d5d4f37a3785660a653d4ff0ad993ee4ab
alexyekymov <alexyekymov@gmail.com>
2020-02-07 23:46:14 +0200

    Client methods implementation.

--- src/com/alex/client/Client.java
+++ src/com/alex/client/Client.java
@@ -1,10 +1,48 @@
 package com.alex.client;
 
 import com.alex.Connection;
+import com.alex.ConsoleHelper;
+import com.alex.Message;
+import com.alex.MessageType;
+
+import java.io.IOException;
 
 public class Client {
     protected Connection connection;
     private volatile boolean clientConnected = false;
+
+    protected String getServerAddress() {
+        ConsoleHelper.writeMessage("Введите адрес сервера:");
+        return ConsoleHelper.readMessage();
+    }
+
+    protected int getServerPort() {
+        ConsoleHelper.writeMessage("Введите порт сервера:");
+        return ConsoleHelper.readInt();
+    }
+
+    protected String getUserName() {
+        ConsoleHelper.writeMessage("Введите Ваше имя:");
+        return ConsoleHelper.readMessage();
+    }
+
+    protected boolean shouldSendTextFromConsole() {
+        return true;
+    }
+
+    protected SocketThread getSocketThread() {
+        return new SocketThread();
+    }
+
+    protected void sendTextMessage(String text) {
+        try {
+            Message message = new Message(MessageType.TEXT, text);
+            connection.send(message);
+        } catch (IOException e) {
+            ConsoleHelper.writeMessage("Не удалось отправить сообщение. " + e.getMessage());
+            clientConnected = false;
+        }
+    }
 
     public class SocketThread extends Thread {
 
