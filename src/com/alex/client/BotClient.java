b2bae43adb42b778143820261f101e0b1fedc516
alexyekymov <alexyekymov@gmail.com>
2020-02-13 23:52:36 +0200

    Chatbot implementation.

--- src/com/alex/client/BotClient.java
+++ src/com/alex/client/BotClient.java
@@ -1,5 +1,11 @@
 package com.alex.client;
 
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Map;
+import java.util.TreeMap;
+
 public class BotClient extends Client {
 
     public static void main(String[] args) {
@@ -25,5 +31,39 @@ public class BotClient extends Client {
 
     public class BotSocketThread extends SocketThread {
 
+        @Override
+        protected void clientMainLoop() throws IOException, ClassNotFoundException {
+            sendTextMessage("Привет чатику. Я бот. Понимаю команды: дата, день, месяц, год, время, час, минуты, секунды.");
+            super.clientMainLoop();
+        }
+
+        @Override
+        protected void processIncomingMessage(String message) {
+            super.processIncomingMessage(message);
+            if (message.contains(": ")) {
+                Map<String, String> map = new TreeMap<>();
+                map.put("дата", "d.MM.yyyy");
+                map.put("день", "d");
+                map.put("месяц", "MMMM");
+                map.put("год", "yyyy");
+                map.put("время", "H:mm:ss");
+                map.put("час", "H");
+                map.put("минуты", "m");
+                map.put("секунды", "s");
+
+                String userName = message.split(": ")[0];
+                String command = message.substring(userName.length() + 2)
+                        .trim();
+                String answer = String.format("Информация для %s: ", userName);
+                LocalDateTime currentTime = LocalDateTime.now();
+                DateTimeFormatter formatter;
+                for (Map.Entry<String, String> pair : map.entrySet()) {
+                    if (pair.getKey().equalsIgnoreCase(command)) {
+                        formatter = DateTimeFormatter.ofPattern(pair.getValue());
+                        sendTextMessage(answer + currentTime.format(formatter));
+                    }
+                }
+            }
+        }
     }
 }
